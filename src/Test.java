bd3dd6b7233cfe65605edea2001389a4b3499ec7
Barun <barunbarjesh@gmail.com>
2024-11-11 17:50:45 +0530

    do some changes

--- src/Test.java
+++ src/Test.java
@@ -3,22 +3,30 @@ import java.util.Comparator;
 
 public class Test {
     public static void main(String[] args) {
-        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        Integer secondHighestNo = Arrays.stream(number)
-                .boxed().sorted(Comparator.reverseOrder())
-                .skip(1).findFirst().get();
+        int[] numbers = {1, 2, 3, 4, 5, -9,6, 7, 8, 9, 9};
+
+        Integer secondHighestNo = Arrays.stream(numbers)
+                .distinct()
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
 
         int temp;
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                if (number[i] > number[j]) {
-                    temp = number[i];
-                    number[i] = number[j];
-                    number[j] = temp;
+        int[] uniqueNumbers = Arrays.stream(numbers)
+                .distinct()
+                .toArray();
+        for (int i = 0; i < uniqueNumbers.length; i++) {
+            for (int j = i + 1; j < uniqueNumbers.length; j++) {
+                if (uniqueNumbers[i] > uniqueNumbers[j]) {
+                    temp = uniqueNumbers[i];
+                    uniqueNumbers[i] = uniqueNumbers[j];
+                    uniqueNumbers[j] = temp;
                 }
             }
         }
-        System.out.println(number[number.length-2]);
-        System.out.println(secondHighestNo);
+        System.out.println("java7 output 2nd Highest No:"+uniqueNumbers[uniqueNumbers.length-2]);
+        System.out.println("java8 output 2nd Highest No:"+secondHighestNo);
     }
 }
